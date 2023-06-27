b2680be8dac8066cc0cf26d16b18774d0f6229af
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-06-27 18:45:10 +0000

    project setup

---
+++ core/shared/constants/index.tsx
@@ -0,0 +1,45 @@
+export const navLinks = [
+  {
+    title: "About Us",
+    url: "/about"
+  },
+  {
+    title: "Service",
+    url: "/services"
+  },
+  {
+    title: "Clients",
+    url: "/clients"
+  },
+  {
+    title: "Contact Us",
+    url: "/contact"
+  }
+]
+
+export const footerLinks = [
+  {
+    title: "About",
+    links: [
+      { title: "About Us", url: "/" },
+      { title: "Why Ghana?", url: "/" },
+      { title: "Our People", url: "/" },
+      { title: "Featured Content", url: "/" },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      { title: "Events", url: "/" },
+      { title: "Blog", url: "/" },
+    ],
+  },
+  {
+    title: "Socials",
+    links: [
+      { title: "Twitter", url: "/" },
+      { title: "Github", url: "/" },
+      { title: "Medium", url: "/" },
+    ],
+  },
+];
