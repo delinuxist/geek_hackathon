4dfcdc0eb1264eda73078bb6de152e4ecd947fa4
Gerald Addo-Tetteh <128140469+gerald-tetteh-tt@users.noreply.github.com>
2023-06-30 12:26:21 +0000

    Change cursor on mobile nav hover and close menu when nav item clicked

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -7,7 +7,7 @@ import { logo } from "../../public/assets/img";
 import { navLinks } from "../../core/shared/constants";
 import { usePathname } from "next/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const pathName = usePathname();
@@ -16,10 +16,13 @@ const Navbar = () => {
   const handleClickMenu = (event: ChangeEvent<HTMLInputElement>) => {
     setMenuOpen(event.target.checked);
   };
+  const handleCloseMenu = () => {
+    setMenuOpen(false);
+  };
 
   return (
     <>
-      <nav className="sticky top-0 bg-software-grey h-14 flex-center z-50">
+      <nav className="sticky top-0 bg-software-grey h-14 flex-center">
         <div className="container mx-auto padding-x flex-between">
           {/* Logo */}
           <div>
@@ -50,29 +53,40 @@ const Navbar = () => {
               );
             })}
           </ul>
-          {/* mobile nav links */}
+          {/* mobile nav button */}
           <input
             onChange={handleClickMenu}
+            checked={menuOpen}
             hidden
             type="checkbox"
             id="menu"
             className="nav-mobile-menu"
           />
-          <label htmlFor="menu">
+          <label htmlFor="menu" className="md:hidden cursor-pointer">
             <FontAwesomeIcon icon={faBars} />
           </label>
         </div>
       </nav>
+      {/* mobile nav items */}
       <nav
-        className={`nav-mobile absolute h-full w-full top-0 left-0 z-10 bg-disruption -translate-x-full transition-all duration-700 ${
+        className={`nav-mobile md:hidden absolute h-full w-full top-0 left-0 z-10 bg-disruption -translate-x-full transition-all duration-700 ${
           menuOpen ? "translate-x-0" : ""
         }`}
       >
+        <div className="nav-mobile-close flex justify-end p-5">
+          <div
+            onClick={handleCloseMenu}
+            className="button-bg bg-white rounded-full flex justify-center cursor-pointer items-center w-14 h-14"
+          >
+            <FontAwesomeIcon size="xl" icon={faXmark} />
+          </div>
+        </div>
         <ul className="mt-28 flex-center flex-col">
           {navLinks.map((link, index) => {
             return (
               <li
                 key={link.title}
+                onClick={handleCloseMenu}
                 className="mr-4 mb-10 last:mr-0 nav-link relative"
               >
                 <Link
