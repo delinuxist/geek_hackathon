ed070ac81d2804ed5902f5b03a15eabacf1947f5
Gerald Addo-Tetteh <128140469+gerald-tetteh-tt@users.noreply.github.com>
2023-06-30 14:51:30 +0000

    Updated general page structure to keep footer at bottom and make main fill available space.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center w-full min-h-screen">
+    <main className="flex flex-col items-center justify-center w-full flex-1">
       <h1>turntabl</h1>
     </main>
-  )
+  );
 }
