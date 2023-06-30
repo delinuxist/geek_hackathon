3623a8f20a41c5a726e33e4a3ec386ed7be626b8
k083k0r3 <kwabena.frempong-kore@turntabl.io>
2023-06-30 00:56:35 +0000

    Jostling Hero Section and adding tid bits

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -3,18 +3,27 @@ import Image from "next/image";
 
 export default function Service() {
     return (
-        <div className="bg-emerald-900 text-white">
-            <section
-                className="container mx-auto py-12 bg-cover bg-center"
-                style={{backgroundImage: `url('/path/to/your-image.jpg')`}}
-            >
-                <div className="text-4xl font-bold">Hero Section</div>
-            </section>
-            <section className="mx-auto py-8">
-                <div className="text-xl font-bold mb-4 text-center">SERVICES</div>
+        <section className="bg-white h-full">
+            <div className="p-12 text-4xl font-bold bg-cover bg-inspiration bg-opacity-50 text-center">
+                <div className="grid grid-cols-2 gap-4 items-center">
+                    <div>Hero Section</div>
+                    <div className='p-12'>
+                        <Image src="https://images.pexels.com/photos/15625905/pexels-photo-15625905/free-photo-of-
+                            close-up-of-a-butterfly-on-a-purple-flower.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                               width={500}
+                               height={500}
+                               alt="Your Image"/>
+                    </div>
+                </div>
+            </div>
+
+            <section className="container mx-auto py-8">
+                <div className="text-pixel-black font-bold text-4xl mb-4 text-center">
+                    <h1>OUR SERVICES</h1>
+                </div>
 
 
-                <div className="p-12 serv-sec mb-2 bg-amber-200">
+                <div className="p-12 serv-sec mb-2 bg-transparent">
                     <div className="grid grid-cols-2 gap-4 items-center">
                         <div>
                             <Image src="https://images.pexels.com/photos/15625905/pexels-photo-15625905/free-photo-of-
@@ -24,8 +33,8 @@ export default function Service() {
                                    alt="Your Image"/>
                         </div>
                         <div>
-                            <h3 className="text-xl font-bold">Partnerships</h3>
-                            <p>
+                            <h3 className="text-pixel-black text-2xl font-bold">Partnerships</h3>
+                            <p className='text-pixel-black'>
                                 Partner with Turntabl today and design low-cost, high-quality technology ethically and
                                 sustainably.
                             </p>
@@ -35,11 +44,11 @@ export default function Service() {
                 <hr className="border-gray-300 my-4 w-full"/>
 
 
-                <div className="p-12 serv-sec mb-2 bg-amber-200">
+                <div className="p-12 serv-sec mb-2 bg-transparent">
                     <div className="grid grid-cols-2 gap-4 items-center">
                         <div>
-                            <h3 className="text-xl font-bold">Project Management</h3>
-                            <p>
+                            <h3 className="text-pixel-black text-2xl font-bold">Project Management</h3>
+                            <p className='text-pixel-black'>
                                 We cooperate with you throughout the project life-cycle in small, committed teams. From
                                 inception and definition to delivery, production and maintenance - our style is clean
                                 lines, efficient code, empowered users.
@@ -57,7 +66,7 @@ export default function Service() {
                 <hr className="border-gray-300 my-4 w-full"/>
 
 
-                <div className="p-12 serv-sec mb-2 bg-amber-200">
+                <div className="p-12 serv-sec mb-2 bg-transparent">
                     <div className="grid grid-cols-2 gap-4 items-center">
                         <div>
                             <Image src="https://images.pexels.com/photos/15625905/pexels-photo-15625905/free-photo-of-
@@ -67,11 +76,10 @@ export default function Service() {
                                    alt="Your Image"/>
                         </div>
                         <div>
-                            <h3 className="text-xl font-bold">Development</h3>
-                            <p>
+                            <h3 className="text-pixel-black text-2xl font-bold">Development</h3>
+                            <p className='text-pixel-black'>
                                 We can assist you in growing your group. Our personnel are adaptable and resourceful. We
-                                chew
-                                through JIRAs just as easily as we develop your UX or next generation micro-service
+                                chew through JIRAs just as easily as we develop your UX or next generation micro-service
                                 layer.
                             </p>
                         </div>
@@ -80,11 +88,11 @@ export default function Service() {
                 <hr className="border-gray-300 my-4 w-full"/>
 
 
-                <div className="p-12 serv-sec mb-2 bg-amber-200">
+                <div className="p-12 serv-sec mb-2 bg-transparent">
                     <div className="grid grid-cols-2 gap-4 items-center">
                         <div>
-                            <h3 className="text-xl font-bold">Software Consultancy</h3>
-                            <p>
+                            <h3 className="text-pixel-black text-2xl font-bold">Software Consultancy</h3>
+                            <p className='text-pixel-black'>
                                 We listen to . your issues first. After that, you get industry-leading services from a
                                 professional solutions team.
                             </p>
@@ -98,10 +106,7 @@ export default function Service() {
                         </div>
                     </div>
                 </div>
-                <hr className="border-gray-300 my-4 w-full"/>
-
-
             </section>
-        </div>
+        </section>
     )
 }
