8549a5d76fcab27e29b7ac88a98a05d5e05617e9
Gerald Addo-Tetteh <128140469+gerald-tetteh-tt@users.noreply.github.com>
2023-06-30 14:57:18 +0000

    remove main from home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main className="flex flex-col items-center justify-center w-full flex-1">
-      <h1>turntabl</h1>
-    </main>
-  );
+  return <h1>turntabl</h1>;
 }
