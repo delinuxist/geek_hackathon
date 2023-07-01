a508cf7b60ec2fad80b216ae43935366e456b72f
Gerald Addo-Tetteh <128140469+gerald-tetteh-tt@users.noreply.github.com>
2023-07-01 17:24:45 +0000

    complete home hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,26 @@
-import Image from "next/image";
 import Link from "next/link";
-import { homeHeroIllustration } from "../public/assets/img";
-import CustomButton from "@/components/utils/CustomButton";
+import HomeHero from "../components/home/home-hero";
 
 export default function Home() {
   return (
     <>
-      <section className="home-hero flex-center h-[668px] bg-center p-3">
-        <div className="home-hero-text flex-1 flex-center flex-col">
-          <h1 className="text-white text-[96px]">
-            <span className="font-neuebit font-bold leading-none">
-              The Next
-            </span>
-            <span className="font-normal leading-none ml-14">Rev¤lution</span>
-            <span className="font-neuebit font-bold leading-5">
-              in Software
-            </span>
-            <span className="font-normal ml-14">Engineering</span>
-          </h1>
-          <div className="home-hero-button-row flex-center">
-            <CustomButton
-              title="About Us"
-              buttonType="button"
-              containerStyles="bg-insightful rounded-full hover:bg-inspiration w-40"
-            ></CustomButton>
-            <Link href="/contact">Get in touch with us</Link>
+      <HomeHero></HomeHero>
+      <section className="home-welcome bg-pixel-black">
+        <div className="container mx-auto padding-x py-20 flex-center flex-col md:flex-row">
+          <div className="home-welcome-text text-white flex-1">
+            <h1>
+              We&apos;re <span>turntabl</span>
+            </h1>
+            <p>
+              From our offices in Ghana and London, we build best-in-class
+              software for the world&apos;s biggest enterprises.
+            </p>
+            <p>Let&apos;s disrupt the status quo together...</p>
+            <Link href="/contact" className="ml-4 text-white">
+              Get in touch &#10230;
+            </Link>
           </div>
-        </div>
-        <div className="home-hero-image flex-1">
-          <Image src={homeHeroIllustration} alt="Home hero image" />
+          <div className="home-welcome-values flex-1"></div>
         </div>
       </section>
     </>
