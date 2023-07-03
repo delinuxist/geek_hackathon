f406b05b9d9d19863a93df2444d2abc4f47869d8
Gerald Addo-Tetteh <128140469+gerald-tetteh-tt@users.noreply.github.com>
2023-07-03 00:35:46 +0000

    add client review slider and add react-slick dependency for slider.

--- components/home/home-clients.tsx
+++ components/home/home-clients.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import {
   fidelityLogo,
   finosLogo,
   msLogo,
   secondStaxLogo,
 } from "../../public/assets/img";
+import Slider from "react-slick";
 import Image from "next/image";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function HomeClients() {
   return (
@@ -35,9 +40,32 @@ export default function HomeClients() {
           width={190}
         />
       </div>
-      <div className="home-client-reviews py-20 ">
-        <div className="home-client-reviews-text"></div>
-        <div className="home-client-reviews-carousel"></div>
+      <div className="home-client-reviews py-20 flex-center gap-10">
+        <div className="home-client-reviews-text self-start">
+          <h3>What our clients say about us</h3>
+          <p>Here it straight from our clients. Real stories, real results.</p>
+        </div>
+        <div className="home-client-reviews-carousel max-w-[700px] w-full">
+          <Slider
+            speed={500}
+            infinite={true}
+            slidesToShow={1}
+            slidesToScroll={1}
+          >
+            <div className="bg-red">
+              <p>1</p>
+            </div>
+            <div className="bg-red">
+              <p>2</p>
+            </div>
+            <div className="bg-red">
+              <p>3</p>
+            </div>
+            <div className="bg-red">
+              <p>4</p>
+            </div>
+          </Slider>
+        </div>
       </div>
     </section>
   );
