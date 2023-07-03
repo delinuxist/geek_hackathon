0c6425b967dd39c51bc28f0e29a1f0b68b5ebe92
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-07-03 21:34:03 +0000

    new assets added

--- public/assets/img/index.ts
+++ public/assets/img/index.ts
@@ -1,4 +1,17 @@
 import logo from "./TT Logo.png";
-import ThumbsUp from "./__Turntabl_Digital_Hand_5-53.png"
+import ThumbsUpIcon from "./__Turntabl_Digital_Hand_5-53.png";
+import SmileIcon from "./smile-icon-green.png";
+import HeroAboutOne from "./hero_about_1.png";
+import HeroAboutTwo from "./hero_about_2.png";
+import HeroAboutThree from "./hero_about_3.png";
+import HeroAboutFour from "./hero_about_4.png";
 
-export { logo,ThumbsUp };
+export {
+  logo,
+  ThumbsUpIcon,
+  SmileIcon,
+  HeroAboutOne,
+  HeroAboutTwo,
+  HeroAboutThree,
+  HeroAboutFour,
+};
