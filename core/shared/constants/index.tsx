43eb558dc589e90bf6fda1f638cfc733e217d23c
k083k0r3 <kwabena.frempong-kore@turntabl.io>
2023-07-04 10:41:38 +0000

    updated footer to include icons for socials

--- core/shared/constants/index.tsx
+++ core/shared/constants/index.tsx
@@ -1,3 +1,5 @@
+import { FaLinkedin, FaTwitter, FaYoutube, FaGithub, FaMedium } from 'react-icons/fa';
+
 export const navLinks = [
   {
     title: "About Us",
@@ -21,27 +23,27 @@ export const footerLinks = [
   {
     title: "About",
     links: [
-      { title: "About Us", url: "/" },
-      { title: "Why Ghana?", url: "/" },
-      { title: "Our People", url: "/" },
-      { title: "Featured Content", url: "/" },
+      { title: "About Us", url: "/", icon: "" },
+      { title: "Why Ghana?", url: "/", icon: "" },
+      { title: "Our People", url: "/", icon: "" },
+      { title: "Featured Content", url: "/", icon: "" },
     ],
   },
   {
     title: "Company",
     links: [
-      { title: "Events", url: "/" },
-      { title: "Blog", url: "/" },
+      { title: "Events", url: "/", icon: "" },
+      { title: "Blog", url: "/", icon: "" },
     ],
   },
   {
     title: "Socials",
     links: [
-      { title: "LindedIn", url: "https://linkedin.com/company/turntabl" },
-      { title: "Twitter", url: "https://twitter.com/turntablio" },
-      { title: "Youtube", url: "https://youtube.com/@turntablgh" },
-      { title: "Github", url: "/" },
-      { title: "Medium", url: "/" },
+      { title: "LinkedIn", url: "https://linkedin.com/company/turntabl", icon: <FaLinkedin /> },
+      { title: "Twitter", url: "https://twitter.com/turntablio", icon: <FaTwitter /> },
+      { title: "Youtube", url: "https://youtube.com/@turntablgh", icon: <FaYoutube /> },
+      { title: "Github", url: "/", icon: <FaGithub /> },
+      { title: "Medium", url: "/", icon: <FaMedium /> },
     ],
   },
 ];
