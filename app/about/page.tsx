f90e7b5e1bdb75573c49ec36fae4e2b949fe044f
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-07-05 07:16:47 +0000

    wrapped tsx with animate presence component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Hero, History, Mission, Founders } from '../../components/pages/about'
+import AnimatePresenceWrapper from '../../components/utils/AnimatePresenceWrapper'
 
 export default function About() {
   return (
-    <main className='relative w-full mt-10 bg-software-grey'>
-      <Hero />
-      <Mission />
-      <History />
-      <Founders />
-    </main>
+    <AnimatePresenceWrapper>
+      <main className='relative w-full mt-10 bg-software-grey'>
+        <Hero />
+        <Mission />
+        <History />
+        <Founders />
+      </main>
+    </AnimatePresenceWrapper>
   )
 }
