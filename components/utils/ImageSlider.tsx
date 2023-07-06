24f8925006b24e741d7944ced92c149af497b379
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-07-06 11:40:20 +0000

    image slider component

--- components/utils/ImageSlider.tsx
+++ components/utils/ImageSlider.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { RxDotFilled } from 'react-icons/rx';
 
@@ -7,24 +7,40 @@ const ImageSlider = () => {
 
   const slides = [
     {
-      url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+      url: '/assets/img/20230301_144951.jpg',
     },
     {
-      url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+      url: '/assets/img/BOwusuNyantekyi_turntabl_pre-edits_20220602_022_V2.jpg',
     },
     {
-      url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+      url: '/assets/img/IMG_9005.jpg',
     },
 
     {
-      url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
+      url: '/assets/img/_MG_3880.jpg',
     },
     {
-      url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
+      url: '/assets/img/IMG_9383.jpg',
     },
   ];
 
+
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const changeImage = setInterval(() => {
+      setCurrentIndex((prev) => {
+        if (prev === slides.length - 1) {
+          return 0;
+        } else {
+          return prev + 1;
+        }
+
+      })
+    }, 10000)
+
+    return () => clearInterval(changeImage)
+  }, [currentIndex, slides.length])
 
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
@@ -45,8 +61,8 @@ const ImageSlider = () => {
 
 
   return (
-    <div className="w-full h-full relative">
-      <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500' style={{ backgroundImage: `url(${slides[currentIndex].url})` }} />
+    <div className="relative w-full h-full">
+      <div className='w-full h-full duration-500 bg-center bg-cover rounded-2xl' style={{ backgroundImage: `url(${slides[currentIndex].url})` }} />
       {/* Left Arrow */}
       <div className=' group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
         <BsChevronCompactLeft onClick={prevSlide} size={30} />
@@ -55,12 +71,12 @@ const ImageSlider = () => {
       <div className=' group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
         <BsChevronCompactRight onClick={nextSlide} size={30} />
       </div>
-      <div className='flex top-4 justify-center py-2'>
+      <div className='flex justify-center py-2 top-4'>
         {slides.map((slide, slideIndex) => (
           <div
             key={slideIndex}
             onClick={() => goToSlide(slideIndex)}
-            className='text-2xl cursor-pointer'
+            className={`text-2xl cursor-pointer duration-400 ease-in-out ${slideIndex === currentIndex && "border-2 rounded-full border-inspiration"}`}
           >
             <RxDotFilled />
           </div>
