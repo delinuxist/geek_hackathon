ee215a6fabe79ea3594e85ee1c68894bbe0117d4
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-07-06 11:42:57 +0000

    updated about page and renamed some images

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Hero, History, Mission, Founders } from '../../components/pages/about'
+import { Hero, History, Mission, Founders, OurTeam } from '../../components/pages/about'
 import AnimatePresenceWrapper from '../../components/utils/AnimatePresenceWrapper'
 
 export default function About() {
   return (
     <AnimatePresenceWrapper>
-      <main className='relative w-full mt-10 bg-software-grey'>
+      <div className='relative w-full mt-10 bg-software-grey'>
         <Hero />
         <Mission />
         <History />
         <Founders />
-      </main>
+        <OurTeam />
+      </div>
     </AnimatePresenceWrapper>
   )
 }
