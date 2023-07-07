1e031b10515a4691d2b430b7128e1d03294686f2
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-07-07 14:42:41 +0000

    display info component added and image slide made reusable

--- components/utils/index.ts
+++ components/utils/index.ts
@@ -1,5 +1,6 @@
 import CustomButton from "./CustomButton";
+import DisplayInfo from "./DisplayInfo";
 import ImageSlider from "./ImageSlider";
 import TransitionEffect from "./TransitionEffect";
 
-export { CustomButton, TransitionEffect, ImageSlider };
+export { CustomButton, TransitionEffect, ImageSlider, DisplayInfo };
