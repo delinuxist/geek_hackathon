0ea13e0b8ccb6c89448984dde109ef21dfaa07c9
k083k0r3 <kwabena.frempong-kore@turntabl.io>
2023-07-09 21:22:06 +0000

    404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,12 +2,17 @@
 
 import React from 'react';
 import Lottie from 'lottie-react';
-import {Error404} from '@/public/assets/json';
+import { Error404 } from '@/public/assets/json';
 
 export default function Custom404() {
     return (
-        <div>
-            <Lottie onComplete={() => {}} animationData={Error404}/>
+        <div className="min-h-[calc(100vh-3.5rem)] flex flex-col justify-center items-center">
+            <h1 className={'text-6xl'}>Page Not Found</h1>
+            {/*<Lottie*/}
+            {/*    onComplete={() => {}}*/}
+            {/*    animationData={Error404}*/}
+            {/*    style={{ height: '65%', width: '65%' }}*/}
+            {/*/>*/}
         </div>
     );
 }
