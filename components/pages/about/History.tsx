447cf8ab3b34fd812b3d5dddd0fd0cc30eb95217
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-07-09 01:47:21 +0000

    adjusted pages max width

--- components/pages/about/History.tsx
+++ components/pages/about/History.tsx
@@ -11,23 +11,25 @@ import 'react-vertical-timeline-component/style.min.css'
 
 const History = () => {
   return (
-    <div className="relative text-white bg-black w-[100%] rounded-b-3xl min-h-screen">
-      <div className="absolute top-0 left-0 w-full h-full ">
-        <Image src={"https://www.webdew.com/hubfs/wdDotted-bg.svg"} fill alt="dotted-bg" className="z-[0.1px] object-cover opacity-5" />
-      </div>
-      <div className="w-[100%] padding-x padding-y z-[100]">
-        <motion.div variants={textVariant(0.1)}>
-          <p className="text-2xl">Company&apos;s History</p>
-          <h1>History</h1>
-        </motion.div>
-        <div className="flex flex-col mt-20">
-          <VerticalTimeline >
-            {
-              histories.map((history) => (
-                <HistoryCard key={history.title} {...history} />
-              ))
-            }
-          </VerticalTimeline>
+    <div className="relative min-h-screen text-white bg-black rounded-b-3xl">
+      <div className="max-w-[100rem] mx-auto">
+        <div className="absolute top-0 left-0 w-full h-full ">
+          <Image src={"https://www.webdew.com/hubfs/wdDotted-bg.svg"} fill alt="dotted-bg" className="z-[0.1px] object-cover opacity-5" />
+        </div>
+        <div className="w-[100%] padding-x padding-y z-[100]">
+          <motion.div variants={textVariant(0.1)}>
+            <p className="text-2xl">Company&apos;s History</p>
+            <h1>History</h1>
+          </motion.div>
+          <div className="flex flex-col mt-20">
+            <VerticalTimeline >
+              {
+                histories.map((history) => (
+                  <HistoryCard key={history.title} {...history} />
+                ))
+              }
+            </VerticalTimeline>
+          </div>
         </div>
       </div>
     </div>
